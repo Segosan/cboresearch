869b3291e546d0a1a8aeb1e98f15b33a66ba0594
Victor <vsegsan@gmail.com>
2021-09-17 09:10:55 +0100

    he

--- people/ana-reyes.php
+++ people/ana-reyes.php
@@ -1,17 +1,13 @@
 
+
 <!DOCTYPE html>
 <html lang="en">
-<?php include '../components/subpages/head.html';?>
+<?php include '../components/head.html';?>
 
 <body>
 <?php include '../components/header.html';?>
 <?php include '../components/half-hero.html';?>
-
-
-
 <?php include '../components/content/people/ana-reyes.html';?>
-
-
 <?php include '../components/footer.html';?>
-<?php include '../components/subpages/scripts.html';?>
+<?php include '../components/scripts.html';?>
 </body>
---
+++ people/ferrao-felipe.php
@@ -0,0 +1,13 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'components/head.html';?>
+
+<body>
+<?php include 'components/header.html';?>
+<?php include 'components/half-hero.html';?>
+<?php include '../components/content/people/ferrao-felipe.html';?>
+<?php include 'components/footer.html';?>
+<?php include 'components/scripts.html';?>
+</body>
